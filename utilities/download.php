fed132b9b8928608468e2c9886c926a1ff889a4c
martinjv9 <martinjv9@gmail.com>
2024-10-28 11:22:41 -0700

    Created download.php to create txt file to download with user info

---
+++ utilities/download.php
@@ -0,0 +1,44 @@
+<?php
+include('../includes/config.php');
+session_start();
+global $pdo;
+
+if (isset($_SESSION["user_id"])) {
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE idusers = ?");
+    $stmt->execute([$_SESSION['user_id']]);
+    $user = $stmt->fetch();
+
+    if ($user) {
+        // Update login count
+        $loginCount = $user['login_count'] + 1;
+
+        // Create personalized file content
+        $content = "Confidential Company Information\n";
+        $content .= "-------------------------------\n";
+        $content .= "Hi, " . $user['first_name'] . " " . $user['last_name'] . "\n";
+        $content .= "You have logged in " . $loginCount . " times.\n";
+        $content .= "Last login date: " . $user['last_login'] . "\n";
+
+        // Set headers for file download
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="company_confidential_file.txt"');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . strlen($content));
+
+        // Output file content
+        echo $content;
+
+        // Update login count, last login, and reset login attempts
+        $updateStmt = $pdo->prepare("UPDATE users SET login_count = login_count + 1, last_login = now(), login_attempts = 0 WHERE idusers = ?");
+        $updateStmt->execute([$user['idusers']]);
+
+        exit;
+    }
+} else {
+    // Redirect to login if the user is not authenticated
+    header("Location: ../views/login.php");
+    exit;
+}
