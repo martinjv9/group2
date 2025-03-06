49ae5ace4dee5356ebbb73d1017a850beef87756
Martin Jimenez <martinjv9@gmail.com>
2025-03-05 22:12:23 -0800

    Security flaws fixed

--- config/config.php
+++ config/config.php
@@ -1,7 +1,8 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';  // Adjusted path
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
 
 return [
     'db' => [
